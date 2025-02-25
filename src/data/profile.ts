bba02417866a67957a0a16c6f3f257b02e17c283
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-02-25 17:47:22 -0300

    feat: included profile data and class

---
+++ src/data/profile.ts
@@ -0,0 +1,22 @@
+export const profile = {
+  eneagram: {
+    name: "O Perfeccionista",
+    code: "Tipo 1",
+    description: "",
+  },
+  disc: {
+    name: "Conformidade - Estabilidade",
+    code: "CS",
+    description: "",
+  },
+  personality16: {
+    name: "Arquiteto",
+    code: "INTJ",
+    description: "",
+  },
+  temperaments: {
+    name: "Melancólico - Pedra",
+    code: "MP",
+    description: "",
+  },
+};
---
+++ src/models/profile.ts
@@ -0,0 +1,22 @@
+export interface Profile {
+  eneagram: {
+    name: string;
+    code: string;
+    description: string;
+  };
+  disc: {
+    name: string;
+    code: string;
+    description: string;
+  };
+  personality16: {
+    name: string;
+    code: string;
+    description: string;
+  };
+  temperaments: {
+    name: string;
+    code: string;
+    description: string;
+  };
+}
