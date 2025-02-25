4917b7d1adaf0ef6869b5470287fea750d6519be
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-02-25 14:59:00 -0300

    style: applied the mobile first technique

--- src/components/common/styles.ts
+++ src/components/common/styles.ts
@@ -8,43 +8,41 @@ export const Header = styled.header`
   top: 0;
   display: flex;
   align-items: center;
+  justify-content: center;
   width: 100%;
   height: 8vh;
-  padding: 0 4rem;
+  padding: 0 1rem;
   background-color: ${(props) => props.theme.colors.background};
   z-index: 4;
 
-  @media ${CONSTANTS.DEVICE.mobileS} {
-    justify-content: center;
-  }
   @media ${CONSTANTS.DEVICE.tablet} {
     justify-content: space-around;
+    padding: 0 2rem;
   }
   @media ${CONSTANTS.DEVICE.desktop} {
     justify-content: space-between;
+    padding: 0 4rem;
   }
 `;
 
-export const Navbar = styled.header`
+export const Navbar = styled.nav`
   display: flex;
-  gap: 25px;
+  gap: 15px;
+
+  @media ${CONSTANTS.DEVICE.tablet} {
+    gap: 25px;
+  }
 `;
 
 export const HeaderLink = styled(NavLink)`
   text-decoration: none;
   display: flex;
   align-items: center;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 10px;
+  padding: 10px 0 10px 10px;
   color: ${(props) => props.theme.colors.white};
   border-radius: 10px;
-  &:hover {
-    color: ${(props) => props.theme.colors.primary};
-    text-shadow: 0 0 10px ${(props) => props.theme.colors.primary},
-      0 0 20px ${(props) => props.theme.colors.primary},
-      0 0 30px ${(props) => props.theme.colors.primary};
-  }
+
+  &:hover,
   &.active {
     color: ${(props) => props.theme.colors.primary};
     text-shadow: 0 0 10px ${(props) => props.theme.colors.primary},
@@ -55,20 +53,13 @@ export const HeaderLink = styled(NavLink)`
 
 export const HeaderScrool = styled.div<{ mobile: boolean }>`
   text-decoration: none;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 10px;
+  padding: 10px 0 10px 10px;
   color: ${(props) => props.theme.colors.white};
   border-radius: 10px;
   cursor: pointer;
   transition: color 0.3s, background-color 0.3s;
 
-  &:hover {
-    color: ${(props) => props.theme.colors.primary};
-    text-shadow: 0 0 10px ${(props) => props.theme.colors.primary},
-      0 0 20px ${(props) => props.theme.colors.primary},
-      0 0 30px ${(props) => props.theme.colors.primary};
-  }
+  &:hover,
   &.active {
     color: ${(props) => props.theme.colors.primary};
     text-shadow: 0 0 10px ${(props) => props.theme.colors.primary},
@@ -76,35 +67,26 @@ export const HeaderScrool = styled.div<{ mobile: boolean }>`
       0 0 30px ${(props) => props.theme.colors.primary};
   }
 
-  @media ${CONSTANTS.DEVICE.mobileS} {
-    display: ${(props) => (props.mobile ? "flex" : "none")};
-    align-items: center;
-  }
+  display: ${(props) => (props.mobile ? "flex" : "none")};
+  align-items: center;
+
   @media ${CONSTANTS.DEVICE.tablet} {
     display: flex;
-    align-items: center;
-  }
-  @media ${CONSTANTS.DEVICE.desktop} {
   }
 `;
 
 // LOGO
 export const Logo = styled.div`
-  height: 20vh;
+  display: none;
 
-  img {
-    width: 100%;
-    height: 100%;
-  }
-
-  @media ${CONSTANTS.DEVICE.mobileS} {
-    display: none;
-  }
   @media ${CONSTANTS.DEVICE.tablet} {
     display: block;
-  }
-  @media ${CONSTANTS.DEVICE.desktop} {
-    display: block;
+    height: 20vh;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 `;
 
@@ -128,11 +110,11 @@ export const FooterList = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 30px;
+  gap: 15px;
 
   span {
     display: flex;
-    gap: 10px;
+    gap: 5px;
   }
   h4 {
     font-family: ${(props) => props.theme.fonts.openSans};
@@ -144,13 +126,13 @@ export const FooterList = styled.div`
     color: ${(props) => props.theme.colors.white};
   }
 
-  @media ${CONSTANTS.DEVICE.mobileS} {
-    div {
-      width: 30px;
-      height: 30px;
-    }
-  }
   @media ${CONSTANTS.DEVICE.tablet} {
+    gap: 30px;
+
+    span {
+      gap: 10px;
+    }
+
     div {
       width: 40px;
       height: 40px;
@@ -161,8 +143,9 @@ export const FooterList = styled.div`
 export const FooterEnd = styled.div`
   width: 100%;
   height: 30%;
-  padding: 0 30px;
+  padding: 0 1rem;
   display: flex;
+  justify-content: center;
   align-items: center;
   background-color: ${(props) => props.theme.colors.background};
   font-family: ${(props) => props.theme.fonts.openSans};
@@ -172,28 +155,25 @@ export const FooterEnd = styled.div`
     color: ${(props) => props.theme.colors.white};
   }
 
-  @media ${CONSTANTS.DEVICE.mobileS} {
-    justify-content: center;
-
-    span {
-      font-size: 0.75rem;
-    }
-    #rules {
-      display: none;
-    }
+  span {
+    font-size: 0.75rem;
   }
+
+  #rules {
+    display: none;
+  }
+
   @media ${CONSTANTS.DEVICE.tablet} {
     justify-content: space-between;
-    gap: 2rem;
+    padding: 0 2rem;
 
     span {
       font-size: 1rem;
     }
+
     #rules {
       display: flex;
       gap: 20px;
     }
-  }
-  @media ${CONSTANTS.DEVICE.desktop} {
   }
 `;
