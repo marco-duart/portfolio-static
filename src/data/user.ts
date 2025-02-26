92a74167d8382d3e28c536bf16ccff462cc10b0b
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-02-26 16:01:41 -0300

    feat: updated user bio data

--- src/data/user.ts
+++ src/data/user.ts
@@ -17,5 +17,42 @@ export const user: User = {
   facebook: "",
   address: "Rua SV-04, nº 27, Sonho Verde, Senador Canedo - GO. 75.260-710",
   bioTitle: "Desenvolvedor FullStack",
-  bio: "Desenvolvedor Fullstack. Possuo forte conhecimento em linguagens como JavaScript e Ruby, e frameworks modernos como NestJS, React e Rails. Minha paixão por tecnologia é alimentada por um desejo contínuo de aprender e evoluir, sempre buscando aplicar e expandir minhas habilidades em desenvolvimento de software. Estou em busca de oportunidades que me permitam contribuir de forma significativa e crescer como profissional.",
+  bio: [
+    {
+      title:
+        "De Aparecida de Goiânia para o Mundo: A Jornada de um Profissional Dedicado",
+      text: "Sou Marco Aurélio, natural de Aparecida de Goiânia, Goiás, e hoje, aos 31 anos, posso dizer que minha trajetória é um reflexo de dedicação, resiliência e paixão pela tecnologia. Minha história começa em uma infância simples, onde aprendi desde cedo o valor do trabalho e da superação.",
+      image: "/assets/images/step1.png",
+    },
+    {
+      title: "O Início: Da Bicicleta ao Primeiro Computador",
+      text: "Na adolescência, descobri minha afinidade com a tecnologia ao realizar pequenas manutenções em computadores. Para ajudar em casa, eu oferecia serviços de 'Manutenção em Domicílio', uma ideia que surgiu da necessidade de me locomover de bicicleta. Esse foi meu primeiro contato com o empreendedorismo e o marketing, conquistando a confiança dos clientes ao levar conveniência e qualidade até suas portas.",
+      image: "/assets/images/step2.png",
+    },
+    {
+      title: "Juventude: Trabalho, Família e Decisões Difíceis",
+      text: "Logo após concluir o ensino médio, entrei no mercado de trabalho para ajudar minha família. Foi um período de muito aprendizado e responsabilidade, onde pude contribuir significativamente em casa. Anos depois, conquistei uma bolsa para Engenharia da Computação, um sonho que começava a se tornar realidade. A paixão pela área floresceu, especialmente em uma aula de algoritmos, ministrada por um professor inspirador. No entanto, a vida me colocou diante de uma escolha difícil: continuar os estudos ou priorizar o sustento da família. Como filho mais velho, optei pelo trabalho, mas a semente da tecnologia já estava plantada em mim.",
+      image: "/assets/images/step3.png",
+    },
+    {
+      title: "Experiências Diversas: Construindo um Repertório Único",
+      text: "Por alguns anos, afastei-me da área de tecnologia, mas cada experiência profissional agregou valor à minha jornada. Trabalhei como monitor de qualidade, onde aprimorei minha capacidade de análise e comunicação, e até atuei como instrutor de treinamento, desenvolvendo habilidades de liderança e didática. Esses momentos foram essenciais para moldar o profissional que sou hoje, mostrando que cada desafio é uma oportunidade de crescimento.",
+      image: "/assets/images/step4.png",
+    },
+    {
+      title: "O Ponto de Virada: Mergulhando de Cabeça na Programação",
+      text: "Em um momento decisivo, resolvi aceitar o risco e investir em um curso imersivo de programação fullstack. Foi um ano intenso, onde prometiam condensar 2 a 3 anos de conteúdo em apenas 12 meses. Cada dia foi um desafio, mas também uma oportunidade de me reinventar. Paralelamente, participei de um processo seletivo para Desenvolvedor na empresa onde trabalhava. Com muita dedicação, entreguei projetos consistentes e conquistei meu espaço na área de tecnologia.",
+      image: "/assets/images/step5.png",
+    },
+    {
+      title: "Hoje: Um Desenvolvedor em Constante Evolução",
+      text: "Atualmente, atuo como Desenvolvedor, onde tenho a oportunidade de aprender diariamente com colegas incríveis e contribuir para projetos significativos. Minha entrada na área pode ter sido tardia, mas minha dedicação e objetivos bem traçados falam mais alto. Acredito que o sucesso é uma combinação de esforço, paixão e resiliência, e estou determinado a alcançar novos patamares nos próximos anos.",
+      image: "/assets/images/step6.png",
+    },
+    {
+      title: "O Futuro: Objetivos e Inspiração",
+      text: "Meu caminho até aqui foi marcado por desafios, mas também por muita determinação. Tenho planos claros para o futuro: continuar evoluindo como profissional, contribuir para a comunidade de tecnologia e inspirar outras pessoas a nunca desistirem de seus sonhos, independentemente do momento em que decidem começar.",
+      image: "/assets/images/step7.png",
+    },
+  ],
 };
